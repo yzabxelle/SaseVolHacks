8cd782d0458f1f71b342863fdbacb1fa385e32f2
Colin <61644709+YearningAsian@users.noreply.github.com>
2023-10-08 16:49:26 -0400

    Gamification
    Leveling System & Unlock System Done.  Needs math integrated.

---
+++ Gamification.py
@@ -0,0 +1,41 @@
+import csv
+
+class Gamification():
+    # Calculates User XP
+    def calculateXP():
+        file = open('dataLog.csv')
+
+        reader = csv.reader(file)
+
+        # Reads from excel file to calculate XP
+        for row in reader:
+            # Math here <<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            totalXP = "something"
+
+        file.close('dataLog.csv')
+        return totalXP
+
+    # Calculates User level
+    def calculateLevel(totalXP):
+        level = 0
+        totalXP = totalXP
+        threshold = 1000
+
+        # Levels up until XP cannot reach newly calculated threshold
+        while (totalXP >= threshold)
+            totalXP -= threshold
+            level += 1
+            threshold = round(threshold * 1.15)
+        
+        return level
+    
+
+    # Unlocks reward if satisfactory level is reached
+    def unlockReward(totalXP, rewardLVL):
+        return (totalXP >= rewardLVL)
+    
+    """
+    def main():
+        Gamification.calculateLevel(calculateXP())
+        Gamification.unlockReward(rewardLVL, calculateXP()) # rewardLVL depends on collectible
+    """
