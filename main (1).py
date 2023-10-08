2bddcefc31283c63e8d55d133479fa81e7b5aee3
WuJason1030 <147265522+WuJason1030@users.noreply.github.com>
2023-10-08 17:53:47 -0400

    Update main (1).py
    - Added EV mode of transport

--- main (1).py
+++ main (1).py
@@ -1,10 +1,11 @@
 #CO2 calculations math part
 
-#intialized the values for car, bus, rail, and carbon neutral of transport
+#intialized the values for car, bus, rail, EV, and carbon neutral modes of transport
 #Grams of carbon diocide per passenger kilometer
 car = 192.0
 bus = 105.0
 neutral = 0.0
+EV = 53
 rail = 41.0
 
 #input from user for kilometers in car
@@ -16,33 +17,39 @@ kmBus = float(input("Kilometers in Bus: "))
 #input from user for kilometers in Rail
 kmRail = float(input("Kilometers in Rail: "))
 
+#input from user for kilometers in EV
+kmEV = float(input("Kilometers in Electric Vehicle: "))
+
 #input from user for kilometers in Carbon Neutral
 kmCarbonNull = float(input("Kilometers in Biking/Walking/Running: "))
 
 #If all modes of travel km is 0 then this will run.
-if kmCar == kmBus == kmRail == kmCarbonNull == 0:
+if kmCar == kmBus == kmRail == kmCarbonNull == kmEV == 0:
   print("Have some walk... Please...")
   exit()
 #Calculating total distance traveled
 #Theorically if distance is traveled by only car
-totalDis = kmCar + kmBus + kmRail + kmCarbonNull
+totalDis = kmCar + kmBus + kmRail + kmCarbonNull + kmEV
 
 #Calculating total CO2 if in car
 totalCO2 = totalDis * car
 
 #Calculating actual CO2 in all modes of transport plus car
 #actualCO2 is the users CO2 emmision
-actualCO2 = (car * kmCar) + (bus * kmBus) + (rail * kmRail) + (neutral * kmCarbonNull)
+actualCO2 = (car * kmCar) + (bus * kmBus) + (rail * kmRail) + (neutral * kmCarbonNull) + (EV * kmEV)
 
 #result of efficency by percentage
-result = round(100 - actualCO2 / totalCO2 * 100, 1)
+result = round(100 - (actualCO2 / totalCO2 * 100), 1)
 
 #If you are 100% efficent then prints 'you are carbon neutral!
 #Congrats! If not then it shows the percentage
 if result == 100:
   print("You are carbon neutral! Congrats!")
 else:
-  print("You are ", result, "% more efficent than just using car to travel!", sep = "")
+  print("You are ",
+        result,
+        "% more efficent than just using car to travel!",
+        sep="")
 
 #Grams of CO2 emmision saved in actual compared to all car in the total CO2
 savedCO2 = int(totalCO2 - actualCO2)
